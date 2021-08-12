f7767ee2893049267cf7d6ff41a3d9f6398c2f27
Lindsey <linds.fonnes@gmail.com>
2021-08-11 21:31:39 -0600

    further study, added reset button

--- src/Eightball.js
+++ src/Eightball.js
@@ -8,12 +8,19 @@ const Eightball = ({answers}) => {
         setMessage(randomAnswer.msg)
         setColor(randomAnswer.color)
     }
+    const reset = () => {
+        setColor("black")
+        setMessage("Think of a Question")
+    }
     const [color, setColor] = useState('black')
     const [message, setMessage]  = useState('Think of a Question')
  return (
+     <>
      <div className={`Eightball-container Eightball-${color}`} onClick={getAnswer}>
          <p>{message}</p>
      </div>
+     <button onClick={reset} className="Eightball-button">Reset</button>
+     </>
  )
 }
 
